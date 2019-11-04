6bbb63795ce95089ebaa4c020e816af07600cffd
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-04 18:05:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,7 +30,7 @@ def array_literal_matrix
   # alphabetically by the first character.
 
   b_matrix = [ENNET_HOUSE.sort,ENFIELD_TENNIS_ACADEMY.sort]
-  
+
 end
 
 def matrix_lookup(matrix, row, column)
